e12eb9656a4d44e876a22ab06cfb1fc470032f87
Rob <rob.adam.h@gmail.com>
2017-03-03 09:16:00 +1100

    single field for date range selected

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
 /*global $, window, document */
 
-var datePicker = document.getElementsByTagName('body')[0];//document.getElementsByClassName('datepicker')[0];
-
-//show date picker on date input click
+var clickOut = document.getElementsByTagName('body')[0];
+/*//show date picker on date input click
 $('.date-input').click(function () {
     $('.datepicker').css('display', 'block');
-});
+});*/
 
+
+
+$('#date').click(function () {
+
+    $.datepicker.setDefaults({
+        dateFormat: "dd/mm/yy",
+        numberOfMonths: 2,
+        minDate: null,
+        maxDate: "+0d",
+        changeYear: true,
+        showButtonPanel: true
+    });
+
+    $("#datepicker").datepicker({
+        beforeShowDay: function (date) {
+            var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").text());
+            var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").text());
+            var isHightlight = date1 && ((date.getTime() == date1.getTime()) || (date2 && date >= date1 && date <= date2));
+            return [true, isHightlight ? "dp-highlight" : ""];
+        },
+        onSelect: function (dateText, inst) {
+            var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").text());
+            var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").text());
+            var selectedDate = $.datepicker.parseDate($.datepicker._defaults.dateFormat, dateText);
+
+            if (!date1 || date2) {
+                $("#input1").text(dateText);
+                $("#input2").text("");
+                $("#date").val(dateText+" - ");
+            } else if (selectedDate < date1) {
+                $("#input2").text($("#input1").text());
+                $("#input1").text(dateText);
+                $("#date").val($("#input1").text()+ " - " +dateText);
+
+            } else {
+                $("#input2").text(dateText);
+                $("#date").val($("#date").val() +dateText);
+            }
+
+            $(this).datepicker();
+
+        }
+    });
+
+
+});
 
 //hide date picker when clicking outside of it
 window.onclick = function(event) {
-    if (event.target == datePicker) {
-        $('.datepicker').css('display', 'none');
+    if (event.target == clickOut) {
+        $('#datepicker').datepicker("destroy");
     }
 };
-
-
-$.datepicker.setDefaults({
-    dateFormat: "dd/mm/yy",
-    numberOfMonths: 2,
-    minDate: null,
-    maxDate: "+0d",
-    changeYear: true,
-    showButtonPanel: true
-});
-
-$(".datepicker").datepicker({
-    beforeShowDay: function (date) {
-        var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
-        var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
-        var isHightlight =
-            date1 && ((date.getTime() == date1.getTime()) || (date2 && date >= date1 && date <= date2));
-        return [true, isHightlight ? "dp-highlight" : ""];
-    },
-    onSelect: function (dateText, inst) {
-        var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
-        var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
-        var selectedDate = $.datepicker.parseDate($.datepicker._defaults.dateFormat, dateText);
-
-        if (!date1 || date2) {
-            $("#input1").val(dateText);
-            $("#input2").val("");
-        } else if (selectedDate < date1) {
-            $("#input2").val($("#input1").val());
-            $("#input1").val(dateText);
-        } else {
-            $("#input2").val(dateText);
-        }
-
-        $(this).datepicker();
-    }
-});
