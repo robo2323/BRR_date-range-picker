29904e2dcdbae4d7490562f9c9ec68c6144782a0
Rob <rob.adam.h@gmail.com>
2017-03-03 13:55:18 +1100

    made responsive and fixed position when on right half of screen

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/*global $, window, document */
+/*global $, document */
 
 
+//when date field is clicked
 $('#date').click(function () {
 
-    $("#modal-clickout-detect").css('display', 'block');
-
+    //initialise datepicker defaults
     $.datepicker.setDefaults({
         dateFormat: "dd/mm/yy",
         numberOfMonths: 2,
@@ -14,7 +14,9 @@ $('#date').click(function () {
         showButtonPanel: true
     });
 
+    //open date picker
     $("#datepicker").datepicker({
+
         beforeShowDay: function (date) {
             var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").text());
             var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").text());
@@ -44,32 +46,32 @@ $('#date').click(function () {
 
         }
     });
+    //if date input field left position is more than half the screen width then aligns datepicker right to date field right
+    if ($('#date').offset().left > $(document).width() / 2) {
 
+        var $offsetWidth = 500-$('#date').width();
+
+        $('.ui-datepicker').css('transform', 'translateX(-'+$offsetWidth+'px)');
+    }
 
 });
 
+//closes date picker when clicking outside it
 $(document).click(function (event) {
 
-    console.log(event.target.tagName);
+    //console.log(event.target.tagName);
 
+    //close if body clicked
     if (event.target.tagName == "BODY") {
         $('#datepicker').datepicker("destroy");
 
     }
 
     event.target.classList.forEach(function (element) {
-        if (!/ui-datepicker*/g.test(element) && !/ui-icon*/g.test(element) && element !== "date-input" && element !== "ui-icon" &&event.target.tagName!=="BUTTON") {
+        //close if element clicked is not ui element of datepicker ***made need tweaking***
+        if (!/ui-datepicker*/g.test(element) && !/ui-icon*/g.test(element) && element !== "date-input" && element !== "ui-icon" && event.target.tagName !== "BUTTON") {
             console.log(element);
             $('#datepicker').datepicker("destroy");
         }
     });
 });
-
-/*
-//hide date picker when clicking outside of it
-window.onclick = function(event) {
-    if (event.target == clickOut) {
-        $('#datepicker').datepicker("destroy");
-    }
-};
-*/
